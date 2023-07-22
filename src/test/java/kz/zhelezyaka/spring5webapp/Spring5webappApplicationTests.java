8a4221128f7db73df2c6b73cce9e28f7b21a688b
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-07-22 22:51:38 +0300

    Start project

---
+++ src/test/java/kz/zhelezyaka/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
